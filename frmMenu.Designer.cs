ae6f0f75c03c98e0c3d680c6a969380f570b8a5d
ADM <ADM@ADM403-25>
2026-01-20 16:48:22 +0700

    Add frmMenu form with MenuStrip for Department and Users
    Introduced a new Windows Form, frmMenu, in the WinFormsApp.Transaction namespace. The form includes a MenuStrip with "Department" and "Users" menu items. Added associated designer and resource files.

---
+++ frmMenu.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WinFormsApp.Transaction
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            departmentToolStripMenuItem = new ToolStripMenuItem();
+            usersToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { departmentToolStripMenuItem, usersToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // departmentToolStripMenuItem
+            // 
+            departmentToolStripMenuItem.Name = "departmentToolStripMenuItem";
+            departmentToolStripMenuItem.Size = new Size(82, 20);
+            departmentToolStripMenuItem.Text = "Department";
+            // 
+            // usersToolStripMenuItem
+            // 
+            usersToolStripMenuItem.Name = "usersToolStripMenuItem";
+            usersToolStripMenuItem.Size = new Size(47, 20);
+            usersToolStripMenuItem.Text = "Users";
+            // 
+            // frmMenu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "frmMenu";
+            Text = "frmMenu";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem departmentToolStripMenuItem;
+        private ToolStripMenuItem usersToolStripMenuItem;
+    }
+}
